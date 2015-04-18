0b91ab1a6b92d2f34d51060e1c70623a270cfc9c
samarth <bansalsamarth@yahoo.com>
2015-04-18 16:37:56 +0530

    settings updated

--- delhi_water_mgmt/settings.py
+++ delhi_water_mgmt/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'analytics',
+    'tastypie',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +82,45 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#---------------Heroku Settings------#
+
+import socket
+if socket.gethostname()!='ubuntu':
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+
+    DATABASES = {
+        "default": {
+           "ENGINE": "django.db.backends.postgresql_psycopg2",
+        }
+    }
+    
+    from os import environ
+    from urlparse import urlparse
+
+    if environ.has_key('DATABASE_URL'):
+        url = urlparse(environ['DATABASE_URL'])
+        DATABASES['default'] = {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+        }
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
+
+    # Static asset configuration
+    import os
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATIC_URL = '/static/'
+
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+        )

f960891bbf82f06b750afe9793f173d77e982f72
samarth <bansalsamarth@yahoo.com>
2015-04-18 15:46:48 +0530

    Initial Models

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Tanker(models.Model):
+	driver = models.ForeignKey(Driver)
+
+class Driver(models.Model):
+	first_name = models.CharField(max_length = 30)
+	last_name = models.CharField(max_length = 30)
+
+class Citizen(models.Model):
+	first_name = models.CharField(max_length = 30)
+	last_name = models.CharField(max_length = 30)
+	address = models.TextField()
+	latitude = models.FloatField()
+	latitude = models.FloatField()
+
+class TrackingData(models.Model):
+	tanker = models.ForeignKey(Tanker)
+	latitude = models.FloatField()
+	longitude = models.FloatField()
+	water_level = models.FloatField()
+	time = models.DateTimeField(auto_now_add = True)
+
+class WaterDispenserData(models.Model):
+	citizen = models.ForeignKey(Citizen)
+	time = models.DateTimeField(auto_now_add = True)
+	tanker = models.ForeignKey(Tanker)
+	amount = models.FloatField()

ef7542d0cbb31b48d575183a2dd9d7d7f447b36c
samarth <bansalsamarth@yahoo.com>
2015-04-18 17:42:08 +0530

    URLs added

--- delhi_water_mgmt/urls.py
+++ delhi_water_mgmt/urls.py
@@ -3,10 +3,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from analytics.api import DriverResource, TrackingResource
+
+v1_api = Api(api_name = 'v1')
+v1_api.register(DriverResource())
+v1_api.register(TrackingResource())
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'delhi_water_mgmt.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(v1_api.urls))
 )

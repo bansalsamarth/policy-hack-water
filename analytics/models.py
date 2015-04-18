1dbffb5b8444a9a0938033ce9a60ffeb4d9a089d
samarth <bansalsamarth@yahoo.com>
2015-04-18 17:44:06 +0530

    Models updated

--- analytics/models.py
+++ analytics/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Tanker(models.Model):
-	driver = models.ForeignKey(Driver)
-
 class Driver(models.Model):
 	first_name = models.CharField(max_length = 30)
 	last_name = models.CharField(max_length = 30)
+
+class Tanker(models.Model):
+	driver = models.ForeignKey(Driver)
 
 class Citizen(models.Model):
 	first_name = models.CharField(max_length = 30)
@@ -19,6 +19,7 @@ class TrackingData(models.Model):
 	latitude = models.FloatField()
 	longitude = models.FloatField()
 	water_level = models.FloatField()
+	speed = models.FloatField()
 	time = models.DateTimeField(auto_now_add = True)
 
 class WaterDispenserData(models.Model):

b20c68f3e6cd3ba1d9452df8e3fad8debaf4100a
samarth <bansalsamarth@yahoo.com>
2015-04-18 19:33:46 +0530

    URLs updated

--- delhi_water_mgmt/urls.py
+++ delhi_water_mgmt/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 from tastypie.api import Api
 
 from analytics.api import DriverResource, TankerResource, CitizenResource, DispensingDataResource, TrackingResource
+from analytics.views import parse_tracking_data, parse_dispension_data
 
 v1_api = Api(api_name = 'v1')
 v1_api.register(DriverResource())
@@ -17,5 +18,7 @@ v1_api.register((DispensingDataResource()))
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(v1_api.urls))
+    url(r'^api/', include(v1_api.urls)),
+    url(r'^dispense/', parse_dispension_data),
+    url(r'^track/', parse_tracking_data)
 )
